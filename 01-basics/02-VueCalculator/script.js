49923a07a939ce8df651e0dc530daad04bf35aa8
magnu <2magnu@gmail.com>
2023-07-31 04:15:35 +0300

    3rd task

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      firstOperand: 0,
+      secondOperand: 0,
+      selectedOperator: null,
+    }
+  },
+
+  computed: {
+    result() {
+      switch (this.selectedOperator) {
+        case "sum":
+          return this.sum(this.firstOperand, this.secondOperand);
+        case "subtract":
+          return this.subtract(this.firstOperand, this.secondOperand);
+        case "multiply":
+          return this.multiply(this.firstOperand, this.secondOperand);
+        case "divide":
+          return this.divide(this.firstOperand, this.secondOperand);
+        default:
+          return 0;
+      }
+    }
+  },
+
+  methods: {
+    sum(a, b) { return a + b },
+    subtract(a, b) { return a - b },
+    multiply(a, b) { return a * b },
+    divide(a, b) { return a / b },
+  },
+
+}).mount('#app');
